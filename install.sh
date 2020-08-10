ab9b756982f5f1e3286a8cf7dae21956cfc84498
Courtney Oka <oakeyc@github.com>
2020-08-10 12:02:09 -0400

    with secrets

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-dotdir=$(cd $(dirname $0) && pwd)
+sym() {
+    dotdir="$1"
 
-for filename in $(find $dotdir -name "*.symlink"); do
-  ln -sf $filename ~/.$(basename $filename | sed "s/\.symlink//")
-done
+    for filename in $(find $dotdir -name "*.symlink"); do
+      ln -sf $filename ~/.$(basename $filename | sed "s/\.symlink//")
+    done
+}
+
+dotfs=$(cd $(dirname $0) && pwd)
+
+sym "$dotfs"
+
+git clone git@github.com:oakeyc/SecretDotfiles.git
+
+./SecretDotfiles/install.sh
+
